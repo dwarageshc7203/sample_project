ac42810da4223449c05d95954f70103aaa677964
dwarageshc7203 <dwarageshc7203@gmail.com>
2026-03-20 08:57:38 +0530

    Removed User Model

--- habit-tracker/src/main/java/com/cnl/habit_tracker/controller/HabitController.java
+++ habit-tracker/src/main/java/com/cnl/habit_tracker/controller/HabitController.java
@@ -1,6 +1,6 @@
 package com.cnl.habit_tracker.controller;
 
-import com.cnl.habit_tracker.models.Habit;
+import com.cnl.habit_tracker.model.Habit;
 import com.cnl.habit_tracker.service.HabitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,29 +16,32 @@ public class HabitController {
     @Autowired
     private HabitService service;
 
-    @PostMapping()
-    public void createHabit(@RequestBody Habit habit) {
+    @PostMapping("/create")
+    public ResponseEntity<Void> createHabit(@RequestBody Habit habit) {
         service.createHabit(habit);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @GetMapping()
+    @GetMapping("habit/{habitId}")
     public ResponseEntity<Habit> getHabit(@PathVariable int habitId) {
         return new ResponseEntity<>(service.getHabit(habitId),HttpStatus.OK );
     }
 
-    @GetMapping()
-    public List<Habit> getAllHabits() {
-        return service.getAllHabits();
+    @GetMapping("habits")
+    public ResponseEntity<List<Habit>> getAllHabits() {
+        return new ResponseEntity<>(service.getAllHabits(), HttpStatus.OK);
     }
 
-    @PutMapping()
-    public void updateHabit(@RequestBody Habit habit) {
+    @PutMapping("update")
+    public ResponseEntity<Void> updateHabit(@RequestBody Habit habit) {
         service.updateHabit(habit);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @DeleteMapping()
-    public void deleteHabit(@PathVariable int habitId) {
+    @DeleteMapping("delete/{habitId}")
+    public ResponseEntity<Void> deleteHabit(@PathVariable int habitId) {
         service.deleteHabit(habitId);
+        return new ResponseEntity<>(HttpStatus.GONE);
     }
 
 }
--- habit-tracker/src/main/java/com/cnl/habit_tracker/repository/HabitRepo.java
+++ habit-tracker/src/main/java/com/cnl/habit_tracker/repository/HabitRepo.java
@@ -1,6 +1,6 @@
 package com.cnl.habit_tracker.repository;
 
-import com.cnl.habit_tracker.models.Habit;
+import com.cnl.habit_tracker.model.Habit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HabitRepo extends JpaRepository<Habit, Integer> {
